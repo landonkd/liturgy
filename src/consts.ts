ba6a0aa6c75140d64baebeda7218271dbd9d2449
Landon Dorrier <landondorrier@Landons-MacBook-Pro.local>
2024-10-21 11:59:15 -0400

    feat: color updates

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Liturgy';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Welcome to a copy/paste of Bema Liturgy with (hopefully) better typography.';
+export const SITE_OG = '/liturgy-og.png';
