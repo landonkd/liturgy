18d494e8e25e974ae5dc5c31d5e8dae797e23382
Landon Dorrier <landondorrier@Landons-MacBook-Pro.local>
2026-01-09 14:48:43 -0500

    feat: design overhaul

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,23 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const liturgy = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		week: z.string(),
-		date: z.coerce.date(), // Transform string to Date object
-		// heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    week: z.string(),
+    date: z.coerce.date(), // Transform string to Date object
+    readings: z
+      .array(
+        z.object({
+          title: z.string(),
+          url: z.string().url().optional(),
+        })
+      )
+      .optional(),
+    // heroImage: z.string().optional(),
+  }),
 });
 
 export const collections = { liturgy };
